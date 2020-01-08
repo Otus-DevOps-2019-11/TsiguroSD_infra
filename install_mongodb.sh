2ca8543fdc50fa281480bb010685f0535cd1c323
TsiguroSD <sofya.tsiguro@gmail.com>
2020-01-08 18:14:14 +0300

    Changed mongodb repo version to Xenial

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -2,8 +2,7 @@
 
 #add key and repo
 sudo wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 #install mongodb
 sudo apt-get update
 sudo apt-get install -y mongodb-org
