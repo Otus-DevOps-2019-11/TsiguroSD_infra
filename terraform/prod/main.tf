1a0460f10d05c952db16739e2c8c3422f33401c2
TsiguroSD <sofya.tsiguro@gmail.com>
2020-03-17 14:54:28 +0300

    updated prod env

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -16,6 +16,8 @@ module "app" {
   public_key_path = var.public_key_path
   zone            = var.zone
   app_disk_image  = var.app_disk_image
+  private_key_path = var.private_key_path
+  db_ip           = module.db.db_external_ip
 }
 
 module "db" {
@@ -23,6 +25,7 @@ module "db" {
   public_key_path = var.public_key_path
   zone            = var.zone
   db_disk_image   = var.db_disk_image
+  private_key_path = var.private_key_path
 }
 
 module "vpc" {
